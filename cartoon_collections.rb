770ea828c8d387314b30f481c71808da9d7d3abf
Trayshawn <traytwebb14@yahoo.com>
2020-11-13 22:19:32 -0600

    commit tree 5aec533a5161b20a84ffb2cdb488172de627ff44

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require "pry"
+
+def roll_call_dwarves(dwarf)
+  roll = []
+  dwarf.each_with_index do|name, index|
+    roll << "#{index+1}. #{name}" 
+    puts roll
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteers)
+  captain = []
+  planeteers.collect do |name| 
+    captain << name.capitalize + "!"
+  end
+  captain
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls(words)
+  words.any? {|i| i.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+
+
+def find_the_cheese(recipe)
+  cheese_types = ["cheddar", "gouda", "camembert"] 
+  recipe.find do |i| 
+    cheese_types.include?(i)
+  end 
 end
